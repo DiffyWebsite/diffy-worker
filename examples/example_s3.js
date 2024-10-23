234e948c0425f96f36deb72170b5973c3f7b229b
markoilicmotive <markoilic.motive@gmail.com>
2024-10-22 19:34:46 -0700

    fix: logger all levels - info, warn, error

--- examples/example_s3.js
+++ examples/example_s3.js
@@ -1,14 +1,17 @@
 
 const process = require('process');
+const debug = !!process.env.DEBUG;
 
+const { Logger } = require('../lib/logger')
 const uploadS3 = require("../lib/uploadS3");
 const filename = '/app/screenshot-1714780252-73939221.webp';
+
+const logger = new Logger(debug);
 
 (async () => {
   s3Url = await uploadS3.upload(filename).catch((err) => {
     throw new Error('Can\'t upload screenshot: ' + err.name + ': ' + (err && Object.hasOwn(err, 'message')) ? err.message : err)
   })
 
-  console.log(s3Url);
-
+  logger.info('S3 URL', s3Url);
 })()
